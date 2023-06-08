2efa3dbb042d521e9066137c7d9886093d4bba15
Karwan87 <damianuszynski@gmail.com>
2023-06-09 00:42:55 +0200

    Update index.js
    deleted imports - deploying failed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import user from './components/user.json';
-import statistics from './components/data.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
