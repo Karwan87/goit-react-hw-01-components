cf5ed74ae4d550c4c70da93177809bb1447508c7
Karwan87 <damianuszynski@gmail.com>
2023-06-12 20:17:05 +0200

    Update App.jsx
    destructurization - User used as a component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,16 @@
-import user from './user.json';
-import Statistics from './Statistics';
-import data from './data.json';
-import FriendList from './FriendList';
-import friends from './friends.json';
-import TransactionsHistory from './TransactionsHistory';
-import transactions from './transactions.json';
+import user from './user/user.json';
+import UserName from './user/UserName.jsx';
+import Statistics from './stats/Statistics';
+import data from './stats/data.json';
+import FriendList from './friends/FriendList';
+import friends from './friends/friends.json';
+import TransactionsHistory from './transactions/TransactionsHistory';
+import transactions from './transactions/transactions.json';
 
 export const App = () => {
   return (
     <div className="profile">
-      <div className="description">
-        <img src={user.avatar} alt="User avatar" className="avatar" />
-        <p className="name">{user.username}</p>
-        <p className="tag">@{user.tag}</p>
-        <p className="location">{user.location}</p>
-      </div>
-
-      <ul className="stats">
-        <li>
-          <span className="label" id="label_1">
-            Followers
-          </span>
-          <span className="quantity">{user.stats.followers}</span>
-        </li>
-        <li>
-          <span className="label" id="label_2">
-            Views
-          </span>
-          <span className="quantity">{user.stats.views}</span>
-        </li>
-        <li>
-          <span className="label" id="label_3">
-            {' '}
-            Likes
-          </span>
-          <span className="quantity">{user.stats.likes}</span>
-        </li>
-      </ul>
+      <UserName user={user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionsHistory items={transactions} />
